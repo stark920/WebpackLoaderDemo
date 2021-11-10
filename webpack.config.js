923e619ffe372d7d3b15dc12638e3efa7fb0b902
stark920 <hautang920@gmail.com>
2021-11-10 23:33:24 +0800

    addHtmlPluginAndLiveServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const { Template } = require('webpack');
       
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
   },
+  mode: 'development',
   module: {
     rules: [
         {
@@ -13,5 +16,8 @@ module.exports = {
             use: ['style-loader', 'css-loader',]
         },
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
 };
